8565c1396737dadac4d126bb7f2ec6cd9aee5260
ZakiaTaou <zakiataou99@gmail.com>
2025-01-05 23:18:37 +0100

    promises task

---
+++ JS/promises.js
@@ -0,0 +1,17 @@
+let promise = new Promise((resolve, reject) => {
+    let time = 1000;
+    if(time === 2000){
+        setTimeout(()=>{
+            resolve("Promise resolved!");
+        }, time);
+    }
+    else{
+        reject("Promise rejected!");
+    }
+})
+promise.then(result=>{
+    console.log(result);
+})
+.catch((error)=>{
+    console.log(error);
+})
